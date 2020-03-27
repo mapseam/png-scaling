9536cffc27f2550af36e92c657839d960f785540
Mapseam <37932909+mapseam@users.noreply.github.com>
2020-03-27 20:23:40 +0400

    Initial version of the file

---
+++ png_resize.php
@@ -0,0 +1,41 @@
+<?php
+  // the file name
+  $srcFileName = 'image.png';
+
+  // type of content
+  header('Content-Type: image/png');
+
+  function pngResize($srcFN, $trgWidth = 0, $trgHeight = 0): bool 
+  {
+    if (($trgWidth < 0) || ($trgHeight < 0)) 
+    {
+      echo "Invalid input parameters!";
+      return false;
+    }
+
+    // get the size of the input image
+    list($srcHeight, $srcHeight) = getimagesize($srcFN); 
+
+    // сalculate the height of the target image if height is not set
+    if ($trgHeight == 0) 
+      $trgHeight = $trgWidth / ($srcWidth / $srcHeight);
+
+    // Calculate the width of the target image if width is not set
+    if ($trgWidth == 0) 
+      $trgWidth = $trgHeight / ($srcHeight / $srcWidth);
+
+    // сreate a handle to the source image
+    $srcHandle = imagecreatefrompng($srcFN);
+   
+    // сreate a handle to the target image
+    $trgHandle = imagecreatetruecolor($trgWidth, $trgHeight);
+
+    // transfer the image from the source to the target, scaling it
+    $result = imagecopyresized($trgHandle, $srcHandle, 0, 0, 0, 0, $trgWidth, $trgHeight, $srcWidth, $srcHeight);
+  
+    return $result and imagepng($trgHandle);
+  }
+
+  // function call
+  pngResize($srcFileName, 200, 100);
+?>
